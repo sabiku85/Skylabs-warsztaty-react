88991092f985de3e9827d6553864bdba9fc1e890
sabiku85 <kurzynska.sabina@gmail.com>
2018-07-14 19:54:49 +0200

    trial

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="all-polls">
+        <ul className="polls-list">
+        <Poll/>
+          <li>
+            <div className="poll-card">
+              <header>
+                <Question/>
+                <Delete/>
+              </header>
+              <div className="content">
+                <ul className="answers">
+                  <li>
+                    <div className="answer">
+                      <p className="title">Yes:<span>1</span></p>
+                      <p className="title">No:<span>3</span></p>
+                      <div className="buttons">
+                        <button className="up">UP</button>
+                        <button className="down">DOWN</button>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     );
+  }
+}
+
+class Poll extends Component {
+  render() {
+    return <h1>Poll</h1>;
+  }
+}
+
+class Question extends Component {
+  render() {
+    return <p className="question">First question</p>;
+  }
+}
+
+class Delete extends Component {
+  render() {
+    return <button className="button">X</button>;
   }
 }
 
